28fce2cfaddf1dcfce1d34b0ebe6c94914621b96
Tim Mauch <tim.mauch@hs-furtwangen.de>
2020-05-29 14:29:52 +0200

    erste Eintrag

---
+++ src/main/java/de/hfu/KleinGross.java
@@ -0,0 +1,16 @@
+package de.hfu;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class KleinGross {
+    public static void main(String[] args) throws IOException  {
+        BufferedReader Reader = new BufferedReader(new InputStreamReader(System.in));
+        String S = Reader.readLine();
+        System.out.println(S.toUpperCase());
+
+    }
+
+}
